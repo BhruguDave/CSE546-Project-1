bb87ee0ce2b8b1a4bbc580c21b573b6ae347c415
Bhrugu Dave <rangbhrugu@gmail.com>
2023-02-25 13:57:56 -0700

    Added response queue polling

--- web_tier/__init__.py
+++ web_tier/__init__.py
@@ -1,39 +1,33 @@
-import json
 from flask import Flask, request, make_response, jsonify
 from werkzeug.utils import secure_filename
 import os
-import time
-from aws_utils import upload_files_to_s3, push_to_sqs, poll_from_sqs
+from aws_utils import upload_files_to_s3, push_to_sqs, poll_from_sqs, get_messages_from_response_queue
 
 # Change URLs and Bucket Name
-request_queue_url = "https://sqs.us-east-1.amazonaws.com/122494296658/app-tier-first"
-response_queue_url = "https://sqs.us-east-1.amazonaws.com/122494296658/app-tier-response"
-bucket_url = "http://s3.amazonaws.com/data-tier-input-bucket/"
-bucket_name = "data-tier-input-bucket"
+request_queue_url = "https://sqs.us-east-1.amazonaws.com/981183274582/ec3_request_queue"
+response_queue_url = "https://sqs.us-east-1.amazonaws.com/981183274582/ec3_response_queue"
+bucket_url = "http://s3.amazonaws.com/cse-546-ec3-input-bucket-1/"
+bucket_name = "cse-546-ec3-input-bucket-1"
 
 app = Flask(__name__)
 
-# response = poll_from_sqs(response_queue_url)
-# print(response)
-# print(response['Messages'][0]['Body'])
 
 @app.route('/', methods=['POST'])
 def upload_files():
+    num_of_incoming_reqs = 0
     for uploaded_file in request.files.getlist('myfile'):
         if uploaded_file.filename != '':
             filename = secure_filename(uploaded_file.filename)
             if filename != '':
-                object_name = str(time.time()) + "$" + filename
+                num_of_incoming_reqs += 1
+                object_name = filename
                 upload_files_to_s3(uploaded_file, bucket_name, object_name)
-                response = push_to_sqs(filename, object_name, request_queue_url)
+                confirmation = push_to_sqs(filename, object_name, request_queue_url)
+                print("Published to Request SQS Queue with MessageId: ", confirmation['MessageId'])
 
-                # response = poll_from_sqs(queue_url);
-
-                print(response['MessageId'])
-
-    # Dummy response
-    data = {'message': 'Done', 'code': 'SUCCESS'}
-    return make_response(jsonify(data), 200)
+    responses = get_messages_from_response_queue(response_queue_url, num_of_incoming_reqs)
+    for response in responses:
+        return make_response(jsonify(response), 200)
 
 
 app.run(
--- web_tier/aws_utils.py
+++ web_tier/aws_utils.py
@@ -1,6 +1,6 @@
 import boto3
 import json
-
+import time
 
 sqs = boto3.client('sqs', region_name='us-east-1')
 s3 = boto3.client('s3')
@@ -35,6 +35,32 @@ def poll_from_sqs(queue_url):
         MessageAttributeNames=[
             'All'
         ],
-        WaitTimeSeconds=20
+        WaitTimeSeconds=1
     )
-    return response
+    if 'Messages' in response:
+        return response['Messages']
+    return []
+
+
+def get_messages_from_response_queue(response_queue_url, num_of_incoming_reqs):
+    responses = []
+    num_of_outgoing_reqs = 0
+    while num_of_outgoing_reqs != num_of_incoming_reqs:
+        messages = poll_from_sqs(response_queue_url)
+        num_of_outgoing_reqs += len(messages)
+        for message in messages:
+            receipt_handle = message['ReceiptHandle']
+            response_body = json.loads(message['Body'])
+            response = {
+                "FileName": response_body['name'] + ".JPEG",
+                "Prediction": response_body['prediction'],
+            }
+            responses.append(response)
+
+            sqs.delete_message(
+                QueueUrl=response_queue_url,
+                ReceiptHandle=receipt_handle
+            )
+        time.sleep(1)
+
+    return responses
